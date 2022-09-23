8dc6516c65f1f194d26f48a12804a006a06623a8
nayr <ryan567.pr@gmail.com>
2022-09-23 02:48:29 -0300

    adicionado efeito suave no scroll da pagina

--- src/indexStyled.tsx
+++ src/indexStyled.tsx
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+html {
+  scroll-behavior: smooth;
+}
+
 body {
   margin: 0;
   font-weight: 500;

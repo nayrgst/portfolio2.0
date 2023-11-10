a7a311c09cd8a0dc1aef21a0168a1d2b4f5bf18b
nayr <ryan567.pr@gmail.com>
2023-11-10 13:38:56 -0300

    alterada a estrura do codigo para vite

--- src/index.tsx
+++ src/index.tsx
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <GlobalStyle />
-    <App />
-    </BrowserRouter>
+      <GlobalStyle />
+        <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
 

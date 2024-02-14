efdf89eedbc2134b1bd314afb0c825adb7409eec
nayr <ryan567.pr@gmail.com>
2024-02-14 19:57:08 -0300

    atualizado o texto da home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -25,8 +25,8 @@ function Home() {
             Olá, sou Pedro Ryan tenho <Age birthdate='2001-10-11'/> anos,<br /> nasci em 
             <strong> Brasília - DF</strong>, e hoje moro em <br /><strong>Águas Lindas - GO </strong>
             junto com meus pais e meu irmão. Sou apaixonado por tecnologia desde
-            que me entendo por gente, agora estou à procura do meu primeiro emprego como desenvolvedor <br/>
-            <strong>Front-End</strong>
+            que me entendo por gente, agora estou à procura do meu primeiro emprego como <br/>
+            <strong>Desenvolvedor</strong>
           </Resume>
        </RightDiv> 
       </PrincipalDiv>
